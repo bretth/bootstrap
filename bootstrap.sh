5271cc03123ffbd51631f647ecc8a73996165df9
Brett Haydon <brett@haydon.id.au>
2016-06-26 21:08:34 +1000

    Add automatic updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 localectl set-locale LANG=en_AU.UTF-8
 timedatectl set-timezone Australia/Sydney
-apt-get --quiet --yes update 
-apt-get -qy upgrade
-apt-get -qy install git python
+apt --quiet --yes update 
+apt -qy upgrade
+apt -qy install git python unattended-upgrades
 git clone https://github.com/bretth/bootstrap
 cp --no-clobber /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 cp -f bootstrap/etc/ssh~sshd_config /etc/ssh/sshd_config
+cp -f bootstrap/etc/apt~apt.conf.d~10periodic /etc/apt/apt.conf.d/10periodic
+cp -f bootstrap/etc/apt~apt.conf.d~50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
+reboot
+
