5bfd5925f1359c294896d13cb3d61b53d985244b
Brett Haydon <brett@haydon.id.au>
2016-06-26 20:43:14 +1000

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
-#!/bin/sh -
+#!/bin/sh
+localectl set-locale LANG=en_AU.UTF-8
+timedatectl set-timezone Australia/Sydney
 apt-get --quiet --yes update 
 apt-get -qy upgrade
 apt-get -qy install git python
+git clone https://github.com/bretth/bootstrap
+cp --no-clobber /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+cp -f bootstrap/etc/ssh~sshd_config /etc/ssh/sshd_config
