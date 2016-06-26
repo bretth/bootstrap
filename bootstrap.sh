f3001e8650088545ccd11646e27cacb2db161ad5
Brett Haydon <brett@haydon.id.au>
2016-06-26 21:49:35 +1000

    salt master

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,3 @@ cp --no-clobber /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 cp -f bootstrap/etc/ssh~sshd_config /etc/ssh/sshd_config
 cp -f bootstrap/etc/apt~apt.conf.d~10periodic /etc/apt/apt.conf.d/10periodic
 cp -f bootstrap/etc/apt~apt.conf.d~50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
-reboot
-
---
+++ master.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -o bootstrap_salt.sh -L https://bootstrap.saltstack.com
+sh bootstrap_salt.sh -M -N git stable
+reboot
